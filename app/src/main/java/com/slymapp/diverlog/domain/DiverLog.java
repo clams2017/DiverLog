6050a92172a6e20c637c5b6765cdc811805c0766
umamo <32873449+umamo@users.noreply.github.com>
2017-12-23 13:50:08 +0900

    DiverLog取得のモッククラス作成

--- app/src/main/java/com/slymapp/diverlog/domain/DiverLog.java
+++ app/src/main/java/com/slymapp/diverlog/domain/DiverLog.java
@@ -77,7 +77,7 @@ public class DiverLog {
     /**
      * 水温
      */
-    private float temperture;
+    private float temperature;
 
     public int getDivingNumber() {
         return divingNumber;
@@ -135,7 +135,70 @@ public class DiverLog {
         return maxDepth;
     }
 
-    public float getTemperture() {
-        return temperture;
+    public float getTemperature() {
+        return temperature;
+    }
+
+    // 以下、モックを作るためのセッター
+    // TODO Builderクラスに以降する
+
+    public void setDivingNumber(int divingNumber) {
+        this.divingNumber = divingNumber;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setEntryMethod(String entryMethod) {
+        this.entryMethod = entryMethod;
+    }
+
+    public void setTransparent(int transparent) {
+        this.transparent = transparent;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStartPressure(int startPressure) {
+        this.startPressure = startPressure;
+    }
+
+    public void setEndPressure(int endPressure) {
+        this.endPressure = endPressure;
+    }
+
+    public void setSuits(String suits) {
+        this.suits = suits;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setAverageDepth(float averageDepth) {
+        this.averageDepth = averageDepth;
+    }
+
+    public void setMaxDepth(float maxDepth) {
+        this.maxDepth = maxDepth;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
     }
 }
