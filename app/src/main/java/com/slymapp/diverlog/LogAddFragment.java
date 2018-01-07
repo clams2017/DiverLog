acf9c93a29a9e8a552765455341472fc44af5393
umamo <32873449+umamo@users.noreply.github.com>
2018-01-07 22:52:52 +0900

    DatePicker上で1月を指定した場合に強制終了する不具合修正

--- app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
+++ app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
@@ -52,7 +52,7 @@ public class LogAddFragment extends Fragment {
                 new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                        binding.logAddDateValue.setText(toDateString(i, i1, i2));
+                        binding.logAddDateValue.setText(toDateString(i, i1 + 1, i2));
                     }
                 }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
                         .show();
