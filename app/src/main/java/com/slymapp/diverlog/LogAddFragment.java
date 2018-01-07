1b8a65800a2dded7730dc0d21b9c9b8ddddfd3c1
umamo <32873449+umamo@users.noreply.github.com>
2018-01-07 22:50:45 +0900

    月指定の不具合修正

--- app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
+++ app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
@@ -76,6 +76,6 @@ public class LogAddFragment extends Fragment {
     private String toDateString(Date date) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        return toDateString(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        return toDateString(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH));
     }
 }
