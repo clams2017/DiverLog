8919a51db5928340916ee236b2364adc967ff42d
umamo <32873449+umamo@users.noreply.github.com>
2018-01-07 22:36:37 +0900

    日付フォーマット変更処理をN以下に対応

--- app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
+++ app/src/main/java/com/slymapp/diverlog/LogAddFragment.java
@@ -3,8 +3,6 @@ package com.slymapp.diverlog;
 import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.databinding.DataBindingUtil;
-import android.icu.text.DateFormat;
-import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -16,9 +14,11 @@ import android.widget.Toast;
 
 import com.slymapp.diverlog.databinding.FragmentLogAddBinding;
 
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 
 /**
@@ -67,15 +67,15 @@ public class LogAddFragment extends Fragment {
         return view;
     }
 
-    // TODO 日付処理をUtil化する
-    @TargetApi(Build.VERSION_CODES.N)
-    private String toDateString(Date date) {
-        DateFormat dateFormat = SimpleDateFormat.getDateInstance();
-        return dateFormat.format(date);
-    }
-
     private String toDateString(int year, int month, int day) {
         // TODO 日付の暫定対応を修正する
-        return toDateString(new GregorianCalendar(year, month, day + 1).getTime());
+        LocalDateTime dateTime = LocalDateTime.of(year, month, day, 0, 0, 0);
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    private String toDateString(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return toDateString(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
     }
 }
