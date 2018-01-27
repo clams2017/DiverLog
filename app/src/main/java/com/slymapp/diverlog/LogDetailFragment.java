96c0ed0812560b1426d76b6da53333e018a14c7f
clams2017 <a@a>
2018-01-27 10:22:15 +0900

    ダイバーログ詳細表示Fragmentの新規作成

--- app/src/main/java/com/slymapp/diverlog/LogDetailFragment.java
+++ app/src/main/java/com/slymapp/diverlog/LogDetailFragment.java
@@ -13,35 +13,21 @@ import android.view.ViewGroup;
  * ダイバーログの詳細表示Fragment
  */
 public class LogDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private int divingNo;
 
-    public LogDetailFragment() {
-        // Required empty public constructor
-    }
-
-    /**
+   /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param divingNo ダイバーログID¥
      * @return A new instance of fragment LogDetailFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static LogDetailFragment newInstance(String param1, String param2) {
+    public static LogDetailFragment newInstance(int divingNo) {
         LogDetailFragment fragment = new LogDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt("divingNumber", divingNo);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +36,7 @@ public class LogDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            divingNo = getArguments().getInt("divingNumber");
         }
     }
 
@@ -60,13 +45,8 @@ public class LogDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_log_detail, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        // TODO
     }
 
     @Override
