a9175c9ac48056b8fef48f27dc2afb56818bc58c
umamo <32873449+umamo@users.noreply.github.com>
2018-01-27 09:50:31 +0900

    [clean] DiverLogRepositoryにjavadoc追加

--- app/src/main/java/com/slymapp/diverlog/domain/DiverLogRepository.java
+++ app/src/main/java/com/slymapp/diverlog/domain/DiverLogRepository.java
@@ -2,8 +2,23 @@ package com.slymapp.diverlog.domain;
 
 import java.util.List;
 
+/**
+ * ダイバーログのリポジトリ
+ */
 public interface DiverLogRepository {
+    /**
+     * ダイバーログを取得します。
+     *
+     * @param divingNo 対象ログのダイビングナンバー
+     * @return {@link DiverLog}
+     */
     DiverLog fetch(int divingNo);
 
+    /**
+     * ダイバーログを全て取得します。
+     *
+     * @return {@link DiverLog}のリスト
+     */
     List<DiverLog> fetchAll();
 }
+
