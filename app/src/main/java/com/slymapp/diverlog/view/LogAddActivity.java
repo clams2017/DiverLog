e3dc08fe5e54b711da9e7b3daa7d703e459ee4ae
clams2017 <a@a>
2018-02-24 16:15:33 +0900

    背景にフォーカスしたときにキーボードが隠れるよう挙動を変更

--- app/src/main/java/com/slymapp/diverlog/view/LogAddActivity.java
+++ app/src/main/java/com/slymapp/diverlog/view/LogAddActivity.java
@@ -3,7 +3,12 @@ package com.slymapp.diverlog.view;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
 
 import com.slymapp.diverlog.R;
 import com.slymapp.diverlog.domain.DiverLog;
@@ -38,7 +43,6 @@ public class LogAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_add);
-
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.log_add_activity_fragment_container, LogAddFragment.newInstance())
                 .commit();
